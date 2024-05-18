bf1f05589fbf669114914a27062c73a8e3dd2052
lotok14 <98856437+lotok14@users.noreply.github.com>
2024-05-19 00:44:53 +0200

    Delete LTKConfiguration directory

--- LTKConfiguration/obj/Debug/netstandard2.0/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace LTKConfiguration
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "LTKConfiguration";
-        public const string PLUGIN_NAME = "LTKConfiguration";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
